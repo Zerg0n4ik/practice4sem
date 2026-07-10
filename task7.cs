0a83b1a4fb1581f8376f4c5d302e3bf0c953a16f
AIStarostin <113984993+Zerg0n4ik@users.noreply.github.com>
2026-07-11 02:38:28 +0300

    Create task7.cs

---
+++ task7.cs
@@ -0,0 +1,30 @@
+﻿using System;
+ 
+class Program
+{
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+ 
+        for (int i = 0; i < t; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(input[0]);
+            int a = int.Parse(input[1]);
+            int b = int.Parse(input[2]);
+ 
+            if (b < 2 * a)
+            {
+                int pairs = n / 2;
+                int remainder = n % 2;
+ 
+                int total = pairs * b + remainder * a;
+                Console.WriteLine(total);
+            }
+            else
+            {
+                Console.WriteLine(n * a);
+            }
+        }
+    }
+}
