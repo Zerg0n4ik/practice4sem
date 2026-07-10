ed767f841468f4b047365cc50cb6302c61d96ced
AIStarostin <113984993+Zerg0n4ik@users.noreply.github.com>
2026-07-11 02:33:58 +0300

    Create task1.cs

---
+++ task1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+ 
+class Program
+{
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+ 
+        for (int test = 0; test < t; test++)
+        {
+            string[] input = Console.ReadLine().Split();
+            int[] a = new int[7];
+            int sum = 0;
+ 
+            for (int i = 0; i < 7; i++)
+            {
+                a[i] = int.Parse(input[i]);
+                sum += a[i];
+            }
+ 
+            int maxSum = int.MinValue;
+            for (int i = 0; i < 7; i++)
+            {
+                int[] temp = new int[7];
+                for (int j = 0; j < 7; j++)
+                {
+                    temp[j] = a[j];
+                }
+ 
+                for (int j = 0; j < 7; j++)
+                {
+                    if (j != i)
+                    {
+                        temp[j] = -temp[j];
+                    }
+                }
+ 
+                int currentSum = 0;
+                for (int j = 0; j < 7; j++)
+                {
+                    currentSum += temp[j];
+                }
+ 
+                if (currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                }
+            }
+ 
+            Console.WriteLine(maxSum);
+        }
+    }
+}
