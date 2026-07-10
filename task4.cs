53e2ffb4071636ed24854ea3ff6a323954ad89a5
AIStarostin <113984993+Zerg0n4ik@users.noreply.github.com>
2026-07-11 02:36:35 +0300

    Create task4.cs

---
+++ task4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+ 
+class Program
+{
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+ 
+        for (int i = 0; i < t; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            int c = int.Parse(input[2]);
+ 
+            for (int j = 0; j < 5; j++)
+            {
+                if (a <= b && a <= c)
+                    a++;
+                else if (b <= a && b <= c)
+                    b++;
+                else
+                    c++;
+            }
+ 
+            Console.WriteLine(a * b * c);
+        }
+    }
+}
