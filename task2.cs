c3e2c35d9b1863f016d265e29b304aaadf08bd46
AIStarostin <113984993+Zerg0n4ik@users.noreply.github.com>
2026-07-11 02:35:12 +0300

    Create task2.cs

---
+++ task2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+ 
+class Program
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        int n = s.Length;
+ 
+        int[] pi = new int[n];
+ 
+        for (int i = 1; i < n; i++)
+        {
+            int j = pi[i - 1];
+ 
+            while (j > 0 && s[i] != s[j])
+            {
+                j = pi[j - 1];
+            }
+ 
+            if (s[i] == s[j])
+            {
+                j++;
+            }
+ 
+            pi[i] = j;
+        }
+ 
+        int periodLength = n - pi[n - 1];
+ 
+        if (n % periodLength == 0)
+        {
+            Console.WriteLine(periodLength);
+        }
+        else
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
